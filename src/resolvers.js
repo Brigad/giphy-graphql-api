77de3af6ed4775d1fb76495346691429081d5203
Adrien HARNAY <adrien@harnay.me>
2019-06-30 14:23:52 +0200

    initial commit

---
+++ src/resolvers.js
@@ -0,0 +1,111 @@
+const camelcase = require('lodash.camelcase');
+const fetch = require('node-fetch');
+const qs = require('qs');
+
+const API_KEY = 'mhvsVNNQYN2u4QqKd8EYvyOVLt9Zu8Ef';
+
+const user = {
+  displayName: 'JoeCool4000',
+  avatarUrl: 'https://media1.giphy.com/avatars/election2016/XwYrZi5H87o6.gif',
+};
+
+const gifWithoutUser = {
+  id: 'YsTs5ltWtEhnq',
+  title: 'Happy Dancing GIF',
+  url: 'http://giphy.com/gifs/confused-flying-YsTs5ltWtEhnq',
+  embedUrl: 'http://giphy.com/embed/YsTs5ltWtEhnq',
+  source:
+    'http://www.reddit.com/r/reactiongifs/comments/1xpyaa/superman_goes_to_hollywood/',
+};
+const gifWithUser = {
+  id: 'YsTs5ltWtEhnq',
+  title: 'Happy Dancing GIF',
+  url: 'http://giphy.com/gifs/confused-flying-YsTs5ltWtEhnq',
+  embedUrl: 'http://giphy.com/embed/YsTs5ltWtEhnq',
+  source:
+    'http://www.reddit.com/r/reactiongifs/comments/1xpyaa/superman_goes_to_hollywood/',
+  user,
+};
+
+const gifs = [gifWithoutUser, gifWithUser];
+
+const stringifyObject = obj => {
+  if (!obj || !Object.keys(obj)) {
+    return '';
+  }
+
+  return qs.stringify(obj);
+};
+
+const stringifyQueryParameters = queryParameters => {
+  const stringified = stringifyObject(queryParameters);
+
+  return stringified ? `?${stringified}` : '';
+};
+
+const camelcaseObjectKeys = object =>
+  Object.entries(object).reduce(
+    (prev, [key, value]) => ({
+      ...prev,
+      [camelcase(key)]:
+        typeof value === 'object' ? camelcaseObjectKeys(value) : value,
+    }),
+    {},
+  );
+
+module.exports = {
+  Query: {
+    gifs: async (_, { search, limit, offset }) => {
+      const query = {
+        api_key: API_KEY,
+        q: search,
+        limit,
+        offset,
+      };
+      const res = await fetch(
+        `https://api.giphy.com/v1/gifs/search${stringifyQueryParameters(
+          query,
+        )}`,
+      );
+      const { data } = await res.json();
+      return data.map(camelcaseObjectKeys);
+    },
+    trendingGifs: async (_, { search, limit, offset }) => {
+      const query = {
+        api_key: API_KEY,
+        limit,
+        offset,
+      };
+      const res = await fetch(
+        `https://api.giphy.com/v1/gifs/trending${stringifyQueryParameters(
+          query,
+        )}`,
+      );
+      const { data } = await res.json();
+      return data.map(camelcaseObjectKeys);
+    },
+    randomGif: async (_, { search }) => {
+      const query = {
+        api_key: API_KEY,
+        tag: search,
+      };
+      const res = await fetch(
+        `https://api.giphy.com/v1/gifs/random${stringifyQueryParameters(
+          query,
+        )}`,
+      );
+      const { data } = await res.json();
+      return camelcaseObjectKeys(data);
+    },
+    gif: async (_, { id }) => {
+      const query = {
+        api_key: API_KEY,
+      };
+      const res = await fetch(
+        `https://api.giphy.com/v1/gifs/${id}${stringifyQueryParameters(query)}`,
+      );
+      const { data } = await res.json();
+      return camelcaseObjectKeys(data);
+    },
+  },
+};
---
+++ src/schema.js
@@ -0,0 +1,31 @@
+const { gql } = require('apollo-server');
+
+module.exports = gql`
+  type User {
+    displayName: String!
+    avatarUrl: String!
+  }
+
+  type Gif {
+    id: String!
+    title: String!
+    url: String!
+    embedUrl: String!
+    source: String
+    user: User
+  }
+
+  type Query {
+    # GIPHY Search gives you instant access to our library of millions of GIFs by entering a word or phrase.
+    gifs(search: String!, limit: Int = 25, offset: Int = 0): [Gif!]!
+
+    # GIPHY Trending returns a list of the most relevant and engaging content each and every day.
+    trendingGifs(limit: Int = 25, offset: Int = 0): [Gif!]!
+
+    # GIPHY Random lets you add some weirdness to the conversation by returning a single random GIF related to the word or phrase entered. If no tag is specified, the GIF returned is completely random.
+    randomGif(search: String): Gif!
+
+    # Get GIF by ID returns a GIF’s metadata based on the GIF ID specified.
+    gif(id: String!): Gif!
+  }
+`;
