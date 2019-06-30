07134c88fb7735492dadd1d96345045308456a40
Adrien HARNAY <adrien@harnay.me>
2019-06-30 14:43:17 +0200

    try setting env

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ const resolvers = require('./resolvers');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || '4000' }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
